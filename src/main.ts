909a1505531b2502f560df60f1351c6538132f86
unknown <zambranojunnior069@gmail.com>
2022-10-11 11:29:18 -0600

    feat: start button

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ICoordinate } from "./intefaces";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const btnStart = document.getElementById("btn__start") as HTMLElement;
 const points: ICoordinate[] = [];
 const padding = 20;
 
@@ -14,8 +15,11 @@ let index = 0;
 let nextApex;
 let checking;
 
+initialize();
+
 function initialize() {
-    for (let i = 0; i < 60; i++) {
+    let maxParticles = Math.floor(Math.random() * (60 - 5) + 5)
+    for (let i = 0; i < maxParticles; i++) {
         let x = Math.floor(Math.random() * (canvas.width - padding - padding) + padding);
         let y = Math.floor(Math.random() * (canvas.height - padding - padding) + padding);
         points.push({ x, y });
@@ -28,12 +32,9 @@ function initialize() {
         ctx.fill();
         ctx.closePath();
     };
-
-    evaluate();
 }
 
 async function evaluate() {
-    await sleep(1000);
     points.sort((a, b) => a.x - b.x);
     leftMost = points[0];
     drawFirstApex(leftMost.x, leftMost.y);
@@ -95,13 +96,13 @@ async function drawComparissonStrokes(
 ) {
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.lineWidth = 0.8;
+    ctx.lineWidth = 0.7;
     ctx.moveTo(currentApex.x, currentApex.y);
     ctx.lineTo(nextApex.x, nextApex.y);
     ctx.stroke();
 };
 
-function drawHull(hull: ICoordinate[]) {
+async function drawHull(hull: ICoordinate[]) {
     for (let i = 0; i < hull.length; i++) {
         ctx.beginPath();
         ctx.strokeStyle = "#0095fffa";
@@ -111,20 +112,29 @@ function drawHull(hull: ICoordinate[]) {
         ctx.stroke();
         ctx.closePath();
 
-        ctx.beginPath();
         ctx.fillStyle = "#ff3232";
         ctx.arc(hull[i].x, hull[i].y, 6, 0, Math.PI * 2);
         ctx.fill();
-        ctx.closePath();
     }
 };
 
-canvas.width = window.innerWidth / 1.2;
-canvas.height = window.innerHeight / 1.2;
+
+btnStart.addEventListener("click", async () => {
+    if (!btnStart.textContent!.includes("Start")) {
+        window.location.reload();
+        return;
+    };
+
+    btnStart.innerText = "Restart";
+    evaluate();
+});
+
+canvas.width = window.innerWidth / 1.3;
+canvas.height = window.innerHeight / 1.3;
 
 window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth / 1.1;
-    canvas.height = window.innerHeight / 1.1;
+    canvas.width = window.innerWidth / 1.3;
+    canvas.height = window.innerHeight / 1.3;
 });
 
 initialize();
